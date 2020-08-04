c54cf8c16a0a4cd7b7ecdf77ba5e25835e22c35b
Felipe <felipe.olivera_@outlook.com>
2020-08-03 23:02:44 -0300

    Teacher list and Teacher form - components

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TeacherForm() {
+    return (
+        <h1>TeacherForm component</h1>
+    );
+}
+
+export default TeacherForm;
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function TeacherList() {
+    return (
+        <h1>TeacherList component</h1>
+    );
+}
+
+export default TeacherList;
